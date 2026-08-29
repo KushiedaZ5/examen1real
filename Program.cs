f9afd81b8a40c7a941bf4024d3c994e8b1bc3e0c
KushiedaZ5 <178656195+KushiedaZ5@users.noreply.github.com>
2026-08-28 21:22:06 -0500

    chore: crear proyecto base ASP.NET Core MVC .NET 10

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using TecnoGasPortal.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Configure EF Core with SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
-
-// Apply migrations automatically on startup
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -35,7 +21,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Solicitudes}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
